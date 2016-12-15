442e1bd73b585906820fe0cf45fcfc7b643ef1c6
mariojmdavid <mariojmdavid@gmail.com>
2016-12-15 22:18:08 +0000

    create the DB

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-
+tempSqlFile='/tmp/ks.sql'
+cat > "$tempSqlFile" <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS keystone ;
+    GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+    GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+    FLUSH PRIVILEGES;
+EOSQL
 
 su -s /bin/sh -c "keystone-manage db_sync" keystone
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
